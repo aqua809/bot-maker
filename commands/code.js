9610c1035334da81908bfd0af7c054eae041bb6e
aqua809 <72797332+aqua809@users.noreply.github.com>
2020-10-25 20:00:56 +0530

    Create code.js

---
+++ commands/code.js
@@ -0,0 +1,8 @@
+//FIRST TEST HANDLER IS WORKING OR NOT
+module.exports = {
+  name: "code",
+  description: "to get code to play together",
+  execute(client, message) {
+    message.channel.send("bclthf = euprope :)");
+  }
+};
