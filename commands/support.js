9815c5436944cafeb223811ae2b0b2f106928fe2
aqua809 <72797332+aqua809@users.noreply.github.com>
2020-10-25 20:06:45 +0530

    Create support.js

---
+++ commands/support.js
@@ -0,0 +1,8 @@
+//FIRST TEST HANDLER IS WORKING OR NOT
+module.exports = {
+  name: "supportus",
+  description: "to support us ",
+  execute(client, message) {
+    message.channel.send("https://discord.gg/7Nq8vBe :)");
+  }
+};
